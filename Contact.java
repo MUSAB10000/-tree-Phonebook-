1e05084b181c9dab06ff6e15600c1babd13abeaf
MUSAB10000 <140024767+MUSAB10000@users.noreply.github.com>
2023-11-17 14:13:13 +0300

    new

---
+++ Contact.java
@@ -0,0 +1,86 @@
+public class Contact implements Comparable<Contact> {
+    private String ContactName;
+    private String PhoneNumber;
+    private String Email;
+    private String address;
+    private String Birthday;
+    private String notes;
+
+    public Contact(String contactName, String phoneNumber, String email, String address, String birthday,
+            String notes) {
+        this.ContactName = contactName;
+        this.PhoneNumber = phoneNumber;
+        this.Email = email;
+        this.address = address;
+        this.Birthday = birthday;
+        this.notes = notes;
+    }
+
+    public Contact(String contactName) {
+        this.ContactName = contactName;
+
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        phoneNumber = PhoneNumber;
+    }
+
+    public String getContactName() {
+        return ContactName;
+    }
+
+    public void setContactName(String contactName) {
+        ContactName = contactName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getBirthday() {
+        return Birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        Birthday = birthday;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    @Override
+    public int compareTo(Contact other) {
+        return this.ContactName.compareTo(other.ContactName);
+    }
+
+    public String toString() {
+        return "\nName: " + ContactName +
+                "\nPhone Number: " + PhoneNumber +
+                "\nEmail Address: " + Email +
+                "\nAddress: " + address +
+                "\nBirthday: " + Birthday +
+                "\nNotes: " + notes + "\n";
+    }
+
+}
---
+++ Event.java
@@ -0,0 +1,66 @@
+public class Event implements Comparable<Event> {
+    private String EventTitle, Location, ContactName;
+    private String DataAndTime;
+    private Contact Contact_inv;
+
+    public Event(String eventTitle, String dateAndTime, String location, Contact Contact_inv) {
+        this.EventTitle = eventTitle;
+        this.DataAndTime = dateAndTime;
+        this.Location = location;
+        this.Contact_inv = Contact_inv;
+        ContactName = Contact_inv.getContactName();
+    }
+
+    public String getEventTitle() {
+        return EventTitle;
+    }
+
+    public void setEventTitle(String eventTitle) {
+        EventTitle = eventTitle;
+    }
+
+    public String getDataAndTime() {
+        return DataAndTime;
+    }
+
+    public void setDataAndTime(String dataAndTime) {
+        DataAndTime = dataAndTime;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
+
+    public String getContactName() {
+        return ContactName;
+    }
+
+    public void setContactName(String contactName) {
+        ContactName = contactName;
+    }
+
+    public Contact getContact_inv() {
+        return Contact_inv;
+    }
+
+    public void setContact_inv(Contact Contact_inv) {
+        this.Contact_inv = Contact_inv;
+    }
+
+    @Override
+    public int compareTo(Event other) {
+        return this.EventTitle.compareTo(other.getEventTitle());
+    }
+
+    public String toString() {
+        String str = "\nEvent title: " + EventTitle +
+                "\nContacts names:   " + ContactName +
+                "\nEvent date and time (MM/DD/YYYY HH:MM): " + DataAndTime +
+                "\nEvent location: " + Location + "\n";
+        return str;
+    }
+}
